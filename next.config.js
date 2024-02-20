b3080eb17a6243fdbb723cca75afdbf5b4c892ea
Stephanie <mills.stephanie.j@gmail.com>
2024-02-20 13:46:45 +0100

    fix avatar images + update config to allow for images from upsplash

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   images: {
-    domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
+     remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+      },
+    ],
+    domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com,`images.unsplash.com'],
   },
 }
